2164ac95959ddf04ae562ea11eab8fb06366b7e8
Farhad Hossain <farhadhossain0085@gmail.com>
2020-11-23 13:39:05 +0600

    context menu should works for mobile device

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
-import { ContextMenu, MenuItem, ContextMenuTrigger } from "react-contextmenu";
-
+import { Menu, Item, contextMenu } from 'react-contexify';
+import 'react-contexify/dist/ReactContexify.min.css';
 
 function ChatMessage({ message, auth, deleteMessage }) {
 
     const { text, uid, photoURL } = message
 
     const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
+
+    const handleMouseDownTextSelection = event => {
+        // user can copy the text but not using double click.
+        if(event.detail > 1){
+            event.preventDefault()
+        }
+    }
+
+    const openContextMenu = e => {
+        e.preventDefault();
+        contextMenu.show({
+            id: message.id,
+            event: e,
+            props: {}
+        });
+    }
 
     return (
         <>
@@ -18,16 +34,13 @@ function ChatMessage({ message, auth, deleteMessage }) {
                     }
                     alt="loading"
                 />
-                <ContextMenuTrigger id={message.id}>
-                    <p>{text}</p>
-                </ContextMenuTrigger>
+                <p onMouseDown={handleMouseDownTextSelection} onDoubleClick={openContextMenu}>{text}</p>
             </div>
 
-            <ContextMenu className="contextMenu" id={message.id}>
-                <MenuItem className="contextMenu item" onClick={() => deleteMessage(message)}>
-                    Delete
-                </MenuItem>
-            </ContextMenu>
+            <Menu id={message.id}>
+                <Item onClick={() => console.log("Edit button clicked")}>Edit</Item>
+                <Item onClick={() => deleteMessage(message)}>Delete</Item>
+            </Menu>
         </>
     )
 }
